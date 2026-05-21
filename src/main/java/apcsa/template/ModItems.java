b98a2c1e6ce531dbd9b2d13836ae52a6a1606115
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2026-05-21 17:41:28 -0400

    Add comments and simplify item registration

--- src/main/java/apcsa/template/ModItems.java
+++ src/main/java/apcsa/template/ModItems.java
@@ -9,15 +9,37 @@ import net.minecraft.world.item.Item;
 
 public class ModItems
 {
+	/**
+	 * Makes an Item with settings
+	 * 
+	 * @param itemSettings The settings for the item
+	 * @return The Item
+	 */
     static Item makeItem(FabricItemSettings itemSettings)
     {
         return new Item(itemSettings);
     }
 
+	/**
+	 * Registers an Item
+	 * 
+	 * @param id The ID of the block (Ie: modid:myitem)
+	 * @param item The Item to be registered
+	 */
+	static void registerItem(String id, Item item)
+	{
+		Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Mod.MOD_ID, id), item);
+	}
+
+	// Items
     public static final Item TEST_ITEM = makeItem(Mod.ITEM_DEFAULT_SETTINGS);
 
+	/**
+	 * Registers all the items
+	 */
     public static void register()
     {
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(Mod.MOD_ID, "test_item"), TEST_ITEM);
+		// Put your items here
+        registerItem("test_item", TEST_ITEM);
     }
 }
